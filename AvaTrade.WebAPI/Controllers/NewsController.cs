a1a18cf2270267d0629dd6de1dd250bb724736f3
Juan David Berovides <juan.david@musala.com>
2023-05-08 20:40:58 +0300

    Add basic news controller endpoint to get all news

---
+++ AvaTrade.WebAPI/Controllers/NewsController.cs
@@ -0,0 +1,22 @@
+﻿using AvaTrade.Microservices.DataStorageService.ServiceContracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AvaTrade.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/news")]
+    public class NewsController : ControllerBase
+    {
+        private readonly INewsService _newsService;
+
+        public NewsController(INewsService newsService) 
+        {
+            _newsService = newsService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous] // TODO: remove this attribute when authentication is implemented
+        public IActionResult Get() => Ok(_newsService.GetAsync().Result);        
+    }
+}
